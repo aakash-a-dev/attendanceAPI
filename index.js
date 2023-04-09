4d7d665f50085b999a507bf6ba3a4fbe28d167cc
aakash-a-dev <aakash.ar1213@gmail.com>
2023-04-09 18:30:30 +0530

    Done with Param in Header

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require('fs');
+
+
 const app = express();
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -16,6 +23,29 @@ app.get("/attendance", (req, res)=>{
     res.send(Atten)
 })
 
+
+
+app.post("/attendance/:name/:time", (req, res) => {
+    const { name, time } = req.params;
+    const newAttendance = { name, time };
+  
+    // Add the new attendance record to the existing array
+    Atten.push(newAttendance);
+  
+    // Write the updated data back to the file
+    try {
+      fs.writeFileSync("Attendance.json", JSON.stringify(Atten), (err) => {
+        if (err) throw err;
+        console.log("Attendance updated successfully");
+      });
+      res.status(200).send("Attendance updated successfully");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error updating attendance");
+    }
+  });
+
+  
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 })
